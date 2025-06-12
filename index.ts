154c87dc48bc6cda1d683a57cbc41bd0d53e6861
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-12 19:37:38 +0300

    added pesudocode to make sure I understood everything correctly :)

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+// writting pesudocode to brainstorm the task before actually writing the code
+//
+// infinite loop till user presses Ctrl+C
+//
+// inside the loop we will have:
+// 1. check if the number is between 1 and 7
+// 2. if the entry is a string or float or any other type other than the type number that has the value of 1 to 7
+// I will use
+// 3. Return the day of the week based on the number and loop again back to the start
+//
+// if step 2 didn't pass we throw an error and be asked to enter a number again
+//
+
+
