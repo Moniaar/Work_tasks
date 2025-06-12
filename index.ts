3c56dc59d564acc0b8bc230acf95d96219156ca8
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-12 20:04:01 +0300

    added days of the week value

--- index.ts
+++ index.ts
@@ -6,9 +6,39 @@
 // 1. check if the number is between 1 and 7
 // 2. if the entry is a string or float or any other type other than the type number that has the value of 1 to 7
 // I will use
+//
 // 3. Return the day of the week based on the number and loop again back to the start
 //
 // if step 2 didn't pass we throw an error and be asked to enter a number again
 //
 
+import { read } from 'read';
 
+async function getdaybynumber() {
+  list const days = [
+  "Sunday": 1;
+  "Monday": 2,
+  "Tuesday": 3,
+  "Wednesday": 4,
+  "Thursday": 5,
+  "Friday": 6,
+  "Saturday": 7
+];
+
+  while (true) {
+    try {
+      const result = await read({prompt: 'Enter a number between 1 and 7:'});
+      if (isNaN(result) || result < 1 || result > 7) {
+        console.error('Invalid input. Please enter a number between 1 and 7.');
+        continue;
+      }
+      else {
+        console.log(`The day of the week is: ${days[result]}`);
+    }
+    catch (error) {
+      console.error('Error reading input:', error);
+    }
+  }
+}
+
+getdaybynumber();
