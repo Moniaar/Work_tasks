72567504661007547e181d589c0b09c69213f393
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-25 10:47:53 +0300

    fixed an importing error

--- task_1/Routing_in_React/src/App.tsx
+++ task_1/Routing_in_React/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from './pages/Navbar'
-import Home from './pages/Home'
 import Projects from './pages/Projects'
 import './App.css'
 
